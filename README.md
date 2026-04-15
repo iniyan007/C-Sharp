f2b473d844ea308b7534a912ab645eef88c11ecc
iniyan007 <iniyanstarzz@gmail.com>
2026-04-15 23:57:10 +0530

    Task-1 completed

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # C-Sharp
+
+## Task-1 Completed ✅
+
+- Basic Data Types, Control Structures, and MethodsObjective:Requirements
---
+++ task-1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("Enter a number");
+            string input = Console.ReadLine();
+            int number = Convert.ToInt32(input);
+            if(number<0)
+            {
+                Console.WriteLine("Negative number entered Please Enter a positivr number");    
+                continue;
+            }
+            else
+            {
+                long factorial = CalculateFactorial(number);
+                Console.WriteLine($"Factorial of {number} is {factorial}");
+            }
+        }
+    }
+    static long CalculateFactorial(int n)
+    {
+        if (n == 0 || n == 1)
+            return 1;
+        else
+            return n * CalculateFactorial(n - 1);
+    }
+}
