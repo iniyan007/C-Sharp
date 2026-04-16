70308bf508c9468e79bf27d3a04c7078cdfad4dc
iniyan007 <iniyanstarzz@gmail.com>
2026-04-16 13:35:52 +0530

    Task-4 completed

---
+++ task-4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace task_four
+{
+    class Student
+    {
+        public String name {get; set;}
+        public int age{get; set;}
+        public double grade{get; set;}
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students= new List<Student>()
+            {
+                new Student { name = "Arun", grade = 85.5, age = 20 },
+                new Student { name = "Bala", grade = 72.3, age = 21 },
+                new Student { name = "Charan", grade = 90.1,age = 19 },
+                new Student { name = "Divya",grade = 88.0,age = 22 },
+                new Student { name = "Esha", grade = 65.4, age = 20 }
+            };
+            double threshold = 80;
+            var filteredStudents = students.Where(s => s.grade>=threshold).OrderBy(s => s.name);
+             Console.WriteLine("Students with Grade above " + threshold + ":\n");
+
+            foreach (var student in filteredStudents)
+            {
+                Console.WriteLine($"Name: {student.name}, Grade: {student.grade}, Age: {student.age}");
+            }
+        }
+    }
+}
---
+++ task-4/README.md
@@ -0,0 +1,19 @@
+# Working with Collections and LINQ
+
+- Create a student management console application.
+- Define a `Student` class with properties such as `Name`, `Grade`, and `Age`.
+- Populate a collection (e.g., a `List<Student>`) with sample data.
+- Use LINQ to:
+    - Filter students who have a grade above a certain threshold.
+    - Sort the filtered results by name or grade.
+- Display the filtered and sorted list.
+
+## Output
+
+```txt
+Students with Grade above 80:
+
+Name: Arun, Grade: 85.5, Age: 20
+Name: Charan, Grade: 90.1, Age: 19
+Name: Divya, Grade: 88, Age: 22
+```
